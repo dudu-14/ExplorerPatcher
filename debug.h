b9523c1aeca784d919175426e3c5b5e4826cdcff
dudu-14 <339035756@qq.com>
2024-11-09 16:27:13 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,3 +1,7 @@
 #define _CRTDBG_MAP_ALLOC
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _SCL_SECURE_NO_WARNINGS
+#define _DEBUG
 #include <stdlib.h>
 #include <crtdbg.h>
